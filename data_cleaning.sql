9113ce0aca5ca9e14166e84121731c03def4d22e
Mors <seyedsahel1383@gmail.com>
2024-07-18 19:03:57 +0330

    find duplicates

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -3,9 +3,23 @@ FROM layoffs_staging;
 SELECT * 
 FROM layoffs;
 
-CREATE TABLE layoffs_staging
-LIKE layoffs;
+-- CREATE TABLE layoffs_staging
+-- LIKE layoffs;
 
-INSERT layoffs_staging
-SELECT *
-FROM layoffs;
+-- INSERT layoffs_staging
+-- SELECT *
+-- FROM layoffs;
+
+WITH duplicate_cte AS (
+SELECT * ,
+ROW_NUMBER() OVER(
+PARTITION BY company , industry , total_laid_off , percentage_laid_off,`date`)
+AS row_num 
+FROM layoffs_staging)
+SELECT * 
+FROM duplicate_cte
+WHERE row_num > 1;
+
+
+
+
