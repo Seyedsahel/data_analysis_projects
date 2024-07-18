417912f3ddb42cf71f86dd62d4ece3047a43f238
Mors <seyedsahel1383@gmail.com>
2024-07-18 21:17:38 +0330

    delete duplicates , done

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -27,3 +27,36 @@ WHERE company = 'casper';
 
 
 
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SELECT * 
+FROM layoffs_staging2;
+
+INSERT INTO layoffs_staging2
+SELECT * ,
+ROW_NUMBER() OVER(
+PARTITION BY company ,location, industry , total_laid_off , percentage_laid_off,`date`,stage,country,funds_raised_millions)
+AS row_num 
+FROM layoffs_staging;
+
+DELETE  
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+
+
