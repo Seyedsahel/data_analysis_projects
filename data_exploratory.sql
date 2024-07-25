84d04f3661196e3552f883edf070df23ef0fb5ca
Mors <seyedsahel1383@gmail.com>
2024-07-25 19:14:34 +0330

    take look at layoffs

--- data_exploratory.sql
+++ data_exploratory.sql
@@ -5,3 +5,22 @@ from layoffs_staging2;
 
 select max(total_laid_off),max(percentage_laid_off)
 from layoffs_staging2;
+
+
+select * 
+from layoffs_staging2
+where percentage_laid_off = 1
+order by total_laid_off desc;
+
+
+select company , sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+# sum of total_laid_off of each company
+
+
+
+
+
+
