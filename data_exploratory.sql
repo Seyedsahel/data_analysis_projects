e3216c35bd3cd35b0894f950368372870097d8e9
Mors <seyedsahel1383@gmail.com>
2024-07-29 15:57:38 +0330

    more exploring , date , countries , etc.

--- data_exploratory.sql
+++ data_exploratory.sql
@@ -17,10 +17,33 @@ select company , sum(total_laid_off)
 from layoffs_staging2
 group by company
 order by 2 desc;
+-- what are you doing with your employees amazon??!!
 # sum of total_laid_off of each company
 
+select min(`date`), max(`date`)
+from layoffs_staging2;
 
 
+select industry , sum(total_laid_off)
+from layoffs_staging2
+group by industry
+order by 2 desc;
+-- HR makes people get fired, then they have the lowest firing rate?!!
+
+select country , sum(total_laid_off)
+from layoffs_staging2
+group by country
+order by 2 desc;
+-- United States don't be rush
+
+select year(`date`) , sum(total_laid_off)
+from layoffs_staging2
+group by year(`date`)
+order by 1 desc;
 
 
-
+select company , avg(percentage_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+-- it doesnt relly help full because we dont know how big the companies are
